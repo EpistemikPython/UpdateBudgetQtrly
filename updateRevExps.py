b00c78f2e497c7829c3968c39c23ac16d2ed5f52
epistemik <epistemik@gmail.com>
2019-03-31 09:32:08 -0400

    successfully get all required revenue and expenses

--- updateRevExps.py
+++ updateRevExps.py
@@ -1,10 +1,13 @@
 #
-# updateBQ_year.py -- use the Gnucash and Google APIs to update my BudgetQtrly document for a specified year
+# updateRevExps.py -- use the Gnucash and Google APIs to update my BudgetQtrly document for a specified year
+#                     in the 'All Inc' and 'Nec Inc' sheets
+#
+# some code from account_analysis.py by Mark Jenkins, ParIT Worker Co-operative <mark@parit.ca>
 #
 # @author Mark Sattolo <epistemik@gmail.com>
 # @version Python 3.6
 # @created 2019-03-30
-# @updated 2019-03-30
+# @updated 2019-03-31
 
 from sys import argv, stdout
 from datetime import date, timedelta, datetime
@@ -12,18 +15,48 @@ from bisect import bisect_right
 from decimal import Decimal
 from math import log10
 import csv
+import json
 from gnucash import Session, GncNumeric
 
-# find the proper path to the account in the gnucash file
+# constant strings
+QTR   = 'quarterly'
+YR    = 'yearly'
+INV   = 'Invest'
+OTH   = 'Other'
+SAL   = 'Salary'
+BAL   = 'Balance'
+CONT  = 'Contingent'
+NEC   = 'Necessary'
+DEDNS = 'Sal_Dedns'
+
+# find the proper path to the accounts in the gnucash file
 REV_ACCTS = {
-    "Invest" : ["REV_Invest"],
-    "Other"  : ["REV_Other"],
-    "Salary" : ["REV_Salary"]
+    INV : ["REV_Invest"],
+    OTH : ["REV_Other"],
+    SAL : ["REV_Salary"]
+}
+EXP_ACCTS = {
+    BAL   : ["EXP_Balance"],
+    CONT  : ["EXP_CONTINGENT"],
+    NEC   : ["EXP_NECESSARY"],
+    DEDNS : ["EXP_Salary"]
+}
+
+# store the sub-totals needed to update the document
+REV_EXP_RESULTS = {
+    INV   : '0',
+    OTH   : '0',
+    SAL   : '0',
+    BAL   : '0',
+    CONT  : '0',
+    NEC   : '0',
+    DEDNS : '0'
 }
 
 # a dictionary with a period name as key, and number of months in that kind of period as the value
 PERIODS = {
-    "quarterly":  3
+    QTR :  3 ,
+    YR  : 12
 }
 
 NUM_MONTHS = 12
@@ -126,25 +159,102 @@ def get_splits(acct, period_starts, period_list):
             period[4] += split_amount
 
 
-# noinspection PyUnresolvedReferences
-def update_bq_year_main():
-    global gnucash_session
+def get_revenue(root_account, period_starts, period_list, re_year, str_quarter):
+    for item in REV_ACCTS:
+        # reset the debit and credit totals for each individual account
+        period_list[0][2] = 0
+        period_list[0][3] = 0
+
+        acct_base = REV_ACCTS[item]
+        # print("acct = {}".format(acct_base))
+
+        account_of_interest = account_from_path(root_account, acct_base)
+        acct_name = account_of_interest.GetName()
+        print("\naccount_of_interest = {}".format(acct_name))
+        # get the split amounts for the parent account
+        get_splits(account_of_interest, period_starts, period_list)
+
+        descendants = account_of_interest.get_descendants()
+        if len(descendants) > 0:
+            # for EACH sub-account add to the overall total
+            # print("Descendants of {}:".format(account_of_interest.GetName()))
+            for subAcct in descendants:
+                # print("{} balance = {}".format(subAcct.GetName(), gnc_numeric_to_python_decimal(subAcct.GetBalance())))
+                get_splits(subAcct, period_starts, period_list)
+
+        # write out the column headers
+        csv_writer = csv.writer(stdout)
+        # csv_writer.writerow('')
+        csv_writer.writerow(('period start', 'period end', 'debits', 'credits', 'TOTAL'))
+
+        # write out the overall totals for the account of interest
+        for start_date, end_date, debit_sum, credit_sum, total in period_list:
+            csv_writer.writerow((start_date, end_date, debit_sum, credit_sum, total))
+
+        sum_revenue = (period_list[0][2] + period_list[0][3]) * (-1)
+        REV_EXP_RESULTS[item] = sum_revenue.to_eng_string()
+        print("{} Revenue for {}{} = ${}".format(acct_name, re_year, str_quarter, sum_revenue))
+
+
+def get_expenses(root_account, period_starts, period_list, re_year, str_quarter):
+    for item in EXP_ACCTS:
+        # reset the debit and credit totals for each individual account
+        period_list[0][2] = 0
+        period_list[0][3] = 0
+
+        acct_base = EXP_ACCTS[item]
+        # print("acct = {}".format(acct_base))
+
+        account_of_interest = account_from_path(root_account, acct_base)
+        acct_name = account_of_interest.GetName()
+        print("\naccount_of_interest = {}".format(acct_name))
+
+        # get the split amounts for the parent account
+        get_splits(account_of_interest, period_starts, period_list)
+
+        descendants = account_of_interest.get_descendants()
+        if len(descendants) > 0:
+            # for EACH sub-account add to the overall total
+            # print("Descendants of {}:".format(account_of_interest.GetName()))
+            for subAcct in descendants:
+                # print("{} balance = {}".format(subAcct.GetName(), gnc_numeric_to_python_decimal(subAcct.GetBalance())))
+                get_splits(subAcct, period_starts, period_list)
+
+        # write out the column headers
+        csv_writer = csv.writer(stdout)
+        # csv_writer.writerow('')
+        csv_writer.writerow(('period start', 'period end', 'debits', 'credits', 'TOTAL'))
+
+        # write out the overall totals for the account of interest
+        for start_date, end_date, debit_sum, credit_sum, total in period_list:
+            csv_writer.writerow((start_date, end_date, debit_sum, credit_sum, total))
+
+        sum_expenses = (period_list[0][2] + period_list[0][3])
+        REV_EXP_RESULTS[item] = sum_expenses.to_eng_string()
+        print("{} Expenses for {}{} = ${}".format(acct_name.split('_')[-1], re_year, str_quarter, sum_expenses))
+
+
+# noinspection PyUnboundLocalVariable,PyUnresolvedReferences
+def update_rev_exps_main():
     exe = argv[0].split('/')[-1]
-    if len(argv) < 4:
+    if len(argv) < 3:
         print("NOT ENOUGH parameters!")
-        print("usage: {} <book url> <year> <quarter>".format(exe))
+        print("usage: {} <book url> <year> [quarter]".format(exe))
         print("PROGRAM EXIT!")
         return
 
     print("\nrunning {} at run-time: {}\n".format(exe, str(datetime.now())))
 
     try:
-        (gnucash_file, str_year, str_quarter) = argv[1:4]
-        print("find Total Revenue in {} for {}-Q{}".format(gnucash_file, str_year, str_quarter))
+        gnucash_file = argv[1]
+        re_year = int(argv[2])
+        period_type = QTR if len(argv) > 3 else YR
 
-        rev_year, rev_quarter = [int(blah) for blah in (str_year, str_quarter)]
+        re_quarter = int(argv[3]) if len(argv) > 3 else 0
+        start_month = (re_quarter * 3) - 2 if len(argv) > 3 else 1
+        str_quarter = '-Q' + str(re_quarter) if re_quarter else ''
 
-        start_month = (rev_quarter * 3) - 2
+        print("find Revenue & Expenses in {} for {}{}".format(gnucash_file, re_year, str_quarter))
 
         # a list of all the periods of interest
         # for each period keep the start date, end date, debits and credits sums and overall total
@@ -153,9 +263,9 @@ def update_bq_year_main():
                 start_date, end_date,
                 ZERO,  # debits sum
                 ZERO,  # credits sum
-                ZERO   # TOTAL
+                ZERO  # TOTAL
             ]
-            for start_date, end_date in generate_period_boundaries(rev_year, start_month, 'quarterly', 1)
+            for start_date, end_date in generate_period_boundaries(re_year, start_month, period_type, 1)
         ]
         # print(period_list)
         # a copy of the above list with just the period start dates
@@ -165,46 +275,22 @@ def update_bq_year_main():
         gnucash_session = Session(gnucash_file, is_new=False)
         root_account = gnucash_session.book.get_root_account()
 
-        for item in REV_ACCTS:
-            # reset the debit and credit totals for each individual account
-            period_list[0][2] = 0
-            period_list[0][3] = 0
-
-            acct_base = REV_ACCTS[item]
-            # print("acct = {}".format(acct_base))
-
-            account_of_interest = account_from_path(root_account, acct_base)
-            acct_name = account_of_interest.GetName()
-            print("\naccount_of_interest = {}".format(acct_name))
-            # get the split amounts for the parent account
-            get_splits(account_of_interest, period_starts, period_list)
-
-            descendants = account_of_interest.get_descendants()
-            if len(descendants) > 0:
-                # for EACH sub-account add to the overall total
-                # print("Descendants of {}:".format(account_of_interest.GetName()))
-                for subAcct in descendants:
-                    # print("{} balance = {}".format(subAcct.GetName(), gnc_numeric_to_python_decimal(subAcct.GetBalance())))
-                    get_splits(subAcct, period_starts, period_list)
-
-            # write out the column headers
-            csv_writer = csv.writer(stdout)
-            # csv_writer.writerow('')
-            csv_writer.writerow(('period start', 'period end', 'debits', 'credits', 'TOTAL'))
-
-            # write out the overall totals for the account of interest
-            for start_date, end_date, debit_sum, credit_sum, total in period_list:
-                csv_writer.writerow((start_date, end_date, debit_sum, credit_sum, total))
-
-            sum_revenue = (period_list[0][2] + period_list[0][3]) * (-1)
-            print("{} Revenue for {}-Q{} = ${}".format(acct_name, str_year, str_quarter, sum_revenue))
-
+        get_revenue(root_account, period_starts, period_list, re_year, str_quarter)
         tot_revenue = period_list[0][4] * (-1)
-        print("\n{} Revenue for {}-Q{} = ${}".format("TOTAL", str_year, str_quarter, tot_revenue))
+        print("\n{} Revenue for {}{} = ${}".format("TOTAL", re_year, str_quarter, tot_revenue))
 
+        period_list[0][4] = 0
+        get_expenses(root_account, period_starts, period_list, re_year, str_quarter)
+        tot_expenses = period_list[0][4]
+        print("\n{} Expenses for {}{} = ${}".format("TOTAL", re_year, str_quarter, tot_expenses))
+
+        print(json.dumps(REV_EXP_RESULTS, indent=4))
         # no save needed, we're just reading..
+
         gnucash_session.end()
+
     except Exception as qe:
+        print("Exception: {}!".format(qe))
         if "gnucash_session" in locals() and gnucash_session is not None:
             gnucash_session.end()
         raise
@@ -213,4 +299,4 @@ def update_bq_year_main():
 
 
 if __name__ == "__main__":
-    update_bq_year_main()
+    update_rev_exps_main()
