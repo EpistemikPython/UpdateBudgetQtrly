141a4093bd24a7b15b14a730b54f9bf4ebcb8a86
epistemik <epistemik@gmail.com>
2019-04-07 16:33:13 -0400

    successully getting values and google file locations; move some items back from updateCommon; &

--- updateAssets.py
+++ updateAssets.py
@@ -1,5 +1,5 @@
 #
-# updateRevExps.py -- use the Gnucash and Google APIs to update the Revenue and Expenses
+# updateRevExps.py -- use the Gnucash and Google APIs to update the Assets
 #                     in my BudgetQtrly document for a specified year or quarter
 #
 # some code from account_analysis.py by Mark Jenkins, ParIT Worker Co-operative <mark@parit.ca>
@@ -7,12 +7,12 @@
 #
 # @author Mark Sattolo <epistemik@gmail.com>
 # @version Python 3.6
-# @created 2019-03-30
-# @updated 2019-04-06
+# @created 2019-04-06
+# @updated 2019-04-07
 
 from sys import argv
 from datetime import datetime as dt
-from gnucash import Session, GncNumeric
+from gnucash import Session
 import pickle
 import os.path as osp
 import copy
@@ -21,72 +21,168 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from updateCommon import *
 
+# constant strings
+QTR   = 'Quarter'
+AU    = 'Gold'
+AG    = 'Silver'
+CASH  = 'Cash'
+BANK  = 'Bank'
+RWRDS = 'Rewards'
+OPEN  = 'OPEN'
+RRSP  = 'RRSP'
+TFSA  = 'TFSA'
+HOUSE = 'House'
+
+# find the proper path to the accounts in the gnucash file
+ASSET_ACCTS = {
+    AU    : ["FAMILY", "Prec Metals", "Au"],
+    AG    : ["FAMILY", "Prec Metals", "Ag"],
+    CASH  : ["FAMILY", "LIQUID", "$&"],
+    BANK  : ["FAMILY", "LIQUID", BANK],
+    RWRDS : ["FAMILY", RWRDS],
+    OPEN  : ["FAMILY", "INVEST", OPEN],
+    RRSP  : ["FAMILY", "INVEST", RRSP],
+    TFSA  : ["FAMILY", "INVEST", TFSA],
+    HOUSE : ["FAMILY", HOUSE]
+}
+
+# store the values needed to update the document
+ASSET_RESULTS = {
+    QTR   : '0',
+    AU    : '0',
+    AG    : '0',
+    CASH  : '0',
+    BANK  : '0',
+    RWRDS : '0',
+    OPEN  : '0',
+    RRSP  : '0',
+    TFSA  : '0',
+    HOUSE : '0'
+}
+
+ASSET_COLS = {
+    AU    : 'U',
+    AG    : 'T',
+    CASH  : 'R',
+    BANK  : 'Q',
+    RWRDS : 'O',
+    OPEN  : 'L',
+    RRSP  : 'M',
+    TFSA  : 'N',
+    HOUSE : 'I'
+}
+BASE_YEAR = 2007
+# number of rows between quarters in the same year
+QTR_SPAN = 1
+# number of rows between years
+BASE_YEAR_SPAN = 4
+
 now = dt.now().strftime("%Y-%m-%dT%H-%M-%S")
+
+
+def year_span(year):
+    """
+    For Asset rows, have to factor in the header row placed every three years
+    :param year: int: year to calculate for
+    :return: int: value to use to calculate which row to update
+    """
+    year_diff = year - BASE_YEAR
+    return (year_diff * BASE_YEAR_SPAN) + (year_diff // 3)
+
+
+def get_acct_bal(acct, idate, cur):
+    # CALLS ARE RETRIEVING ASSET VALUES FROM DAY BEFORE!!??
+    idate += ONE_DAY
+
+    acct_bal = acct.GetBalanceAsOfDate(idate)
+    acct_comm = acct.GetCommodity()
+    # print_info("acct_comm = {}".format(acct_comm))
+    if acct_comm == cur:
+        acct_cad = acct_bal
+    else:
+        acct_cad = acct.ConvertBalanceToCurrencyAsOfDate(acct_bal, acct_comm, cur, idate)
+    print_info("{} balance on {} = {}".format(acct.GetName(), idate, acct_cad))
+
+    return gnc_numeric_to_python_decimal(acct_cad)
+
+
+def get_acct_totals(root_account, end_date, cur):
+    """
+    Get REVENUE data for the specified account for the specified quarter
+    :param root_account: Gnucash account: from the Gnucash book
+    :param end_date: date: read the account total at the end of the quarter
+    :param cur: Gnucash Commodity: currency to use for the totals
+    :return: string with sum of totals
+    """
+    str_sum = 'EMPTY'
+    for item in ASSET_ACCTS:
+        path = ASSET_ACCTS[item]
+        acct = account_from_path(root_account, path)
+        acct_name = acct.GetName()
+
+        # get the split amounts for the parent account
+        acct_sum = get_acct_bal(acct, end_date, cur)
+        descendants = acct.get_descendants()
+        if len(descendants) > 0:
+            # for EACH sub-account add to the overall total
+            # print("Descendants of {}:".format(acct_name))
+            for sub_acct in descendants:
+                # ?? GETTING SLIGHT ROUNDING ERRORS WHEN ADDING MUTUAL FUND VALUES...
+                acct_sum += get_acct_bal(sub_acct, end_date, cur)
+
+        str_sum = acct_sum.to_eng_string()
+        print_info("Assets for {} on {} = ${}\n".format(acct_name, end_date, str_sum), MAGENTA)
+        ASSET_RESULTS[item] = str_sum
+
+    return str_sum
 
 
 # noinspection PyUnboundLocalVariable,PyUnresolvedReferences
 def get_assets(gnucash_file, re_year, re_quarter):
     """
-    Get REVENUE and EXPENSE data for ONE specified Quarter or ALL four Quarters for the specified Year
+    Get ASSET data for ONE specified Quarter or ALL four Quarters for the specified Year
     :param gnucash_file: string: name of file used to read the values
     :param re_year: int: year to update
     :param re_quarter: int: 1..4 for quarter to update or 0 if updating entire year
     :return: nil
     """
     num_quarters = 1 if re_quarter else 4
-    print("find Revenue & Expenses in {} for {}{}".format(gnucash_file, re_year, ('-Q' + str(re_quarter)) if re_quarter else ''))
+    print_info("find Assets in {} for {}{}".format(gnucash_file, re_year, ('-Q' + str(re_quarter)) if re_quarter else ''), GREEN)
 
     try:
         gnucash_session = Session(gnucash_file, is_new=False)
         root_account = gnucash_session.book.get_root_account()
-        # print("type root_account = {}".format(type(root_account)))
+        commod_tab = gnucash_session.book.get_table()
+        # noinspection PyPep8Naming
+        CAD = commod_tab.lookup("ISO4217", "CAD")
 
         for i in range(num_quarters):
+            acct_total = ZERO
             qtr = re_quarter if re_quarter else i + 1
-            REV_EXP_RESULTS[QTR] = str(qtr)
+            ASSET_RESULTS[QTR] = str(qtr)
 
             start_month = (qtr * 3) - 2
+            end_date = period_end(re_year, start_month)
 
-            # for each period keep the start date, end date, debits and credits sums and overall total
-            period_list = [
-                [
-                    start_date, end_date,
-                    ZERO, # debits sum
-                    ZERO, # credits sum
-                    ZERO  # TOTAL
-                ]
-                for start_date, end_date in generate_period_boundaries(re_year, start_month, QTR, 1)
-            ]
-            # print(period_list)
-            # a copy of the above list with just the period start dates
-            period_starts = [e[0] for e in period_list]
-            # print(period_starts)
+            acct_total = get_acct_totals(root_account, end_date, CAD)
+            # print("\n{} Assets for {}-Q{} = ${}".format("TOTAL", re_year, qtr, acct_total))
 
-            get_revenue(root_account, period_starts, period_list, re_year, qtr)
-            tot_revenue = period_list[0][4] * (-1)
-            print("\n{} Revenue for {}-Q{} = ${}".format("TOTAL", re_year, qtr, tot_revenue))
-
-            period_list[0][4] = 0
-            get_expenses(root_account, period_starts, period_list, re_year, qtr)
-            tot_expenses = period_list[0][4]
-            print("\n{} Expenses for {}-Q{} = ${}\n".format("TOTAL", re_year, qtr, tot_expenses))
-
-            results.append(copy.deepcopy(REV_EXP_RESULTS))
-            period_list[0][4] = 0
-            print(json.dumps(REV_EXP_RESULTS, indent=4))
+            results.append(copy.deepcopy(ASSET_RESULTS))
+            # print(json.dumps(ASSET_RESULTS, indent=4))
 
         # no save needed, we're just reading...
         gnucash_session.end()
 
-        save_to_json('out/updateRevExps_results', results)
+        save_to_json('out/updateAssets_results', now, results)
 
-    except Exception as qe:
-        print("Exception: {}!".format(qe))
+    except Exception as ge:
+        print_error("Exception: {}!".format(ge))
         if "gnucash_session" in locals() and gnucash_session is not None:
             gnucash_session.end()
+        exit(177)
 
 
-def fill_rev_exps_data(mode, re_year):
+def fill_assets_data(mode, re_year):
     """
     Fill the data list:
     for each item in results, either 1 for one quarter or 4 for four quarters:
@@ -101,35 +197,29 @@ def fill_rev_exps_data(mode, re_year):
     :param re_year: int: year to update
     :return: data list
     """
-    print("\nfill_rev_exps_data({}, {})\n".format(mode, re_year))
+    print_info("\nfill_assets_data({}, {})\n".format(mode, re_year), CYAN)
 
-    all_inc_dest = ALL_INC_PRAC_SHEET
-    nec_inc_dest = NEC_INC_PRAC_SHEET
+    dest = QTR_ASTS_PRAC_SHEET
     if 'prod' in mode:
-        all_inc_dest = ALL_INC_SHEET
-        nec_inc_dest = NEC_INC_SHEET
-    print("all_inc_dest = {}".format(all_inc_dest))
-    print("nec_inc_dest = {}\n".format(nec_inc_dest))
+        dest = QTR_ASTS_SHEET
+    print_info("dest = {}\n".format(dest))
 
-    year_row = BASE_ROW + ((re_year - RE_BASE_YEAR) * RE_YEAR_SPAN)
+    year_row = BASE_ROW + year_span(re_year)
     # get exact row from Quarter value in each item
     for item in results:
-        print("{} = {}".format(QTR, item[QTR]))
+        print_info("{} = {}".format(QTR, item[QTR]))
         int_qtr = int(item[QTR])
-        dest_row = year_row + ((int_qtr - 1) * RE_QTR_SPAN)
-        print("dest_row = {}\n".format(dest_row))
+        dest_row = year_row + ((int_qtr - 1) * QTR_SPAN)
+        print_info("dest_row = {}\n".format(dest_row))
         for key in item:
             if key != QTR:
                 cell = copy.copy(cell_data)
-                dest = nec_inc_dest
-                if key in (REV, BAL, CONT):
-                    dest = all_inc_dest
-                col = REV_EXP_COLS[key]
+                col = ASSET_COLS[key]
                 val = item[key]
                 cell_locn = dest + '!' + col + str(dest_row)
                 cell['range']  = cell_locn
                 cell['values'] = [[val]]
-                print("cell = {}".format(cell))
+                print_info("cell = {}".format(cell))
                 data.append(cell)
     return data
 
@@ -141,61 +231,52 @@ def send_assets(mode, re_year):
     :param re_year: int: year to update
     :return: server response
     """
-    print("\nsend_rev_exps({}, {})".format(mode, re_year))
-    print("cell_data['range'] = {}".format(cell_data['range']))
-    print("cell_data['values'][0][0] = {}".format(cell_data['values'][0][0]))
-
-    fill_rev_exps_data(mode, re_year)
-
-    save_to_json('out/updateRevExps_data', data)
-
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time.
-    if osp.exists(TOKEN):
-        with open(TOKEN, 'rb') as token:
-            creds = pickle.load(token)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS, SHEETS_RW_SCOPE)
-            creds = flow.run_local_server()
-        # Save the credentials for the next run
-        with open(TOKEN, 'wb') as token:
-            pickle.dump(creds, token, pickle.HIGHEST_PROTOCOL)
-
-    # Call the Sheets API
-    service = build('sheets', 'v4', credentials=creds)
-    srv_sheets = service.spreadsheets()
-
-    my_body = {
-        'valueInputOption': 'USER_ENTERED',
-        'data': data
-    }
+    print_info("\nsend_assets({}, {})".format(mode, re_year))
+    print_info("cell_data['range'] = {}".format(cell_data['range']))
+    print_info("cell_data['values'][0][0] = {}".format(cell_data['values'][0][0]))
 
     response = 'NO SEND.'
-    if 'send' in mode:
-        vals = srv_sheets.values()
-        response = vals.batchUpdate(spreadsheetId=BUDGET_QTRLY_SPRD_SHEET, body=my_body).execute()
+    try:
+        fill_assets_data(mode, re_year)
 
-        print('\n{} cells updated!'.format(response.get('totalUpdatedCells')))
-        save_to_json('out/updateRevExps_response', response)
+        save_to_json('out/updateAssets_data', now, data)
+
+        creds = None
+        if osp.exists(TOKEN):
+            with open(TOKEN, 'rb') as token:
+                creds = pickle.load(token)
+
+        # if there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS, SHEETS_RW_SCOPE)
+                creds = flow.run_local_server()
+            # save the credentials for the next run
+            with open(TOKEN, 'wb') as token:
+                pickle.dump(creds, token, pickle.HIGHEST_PROTOCOL)
+
+        service = build('sheets', 'v4', credentials=creds)
+        srv_sheets = service.spreadsheets()
+
+        my_body = {
+            'valueInputOption': 'USER_ENTERED',
+            'data': data
+        }
+
+        if 'send' in mode:
+            vals = srv_sheets.values()
+            response = vals.batchUpdate(spreadsheetId=BUDGET_QTRLY_SPRD_SHEET, body=my_body).execute()
+
+            print_info('\n{} cells updated!'.format(response.get('totalUpdatedCells')))
+            save_to_json('out/updateAssets_response', now, response)
+
+    except Exception as se:
+        print_error("Exception: {}!".format(se))
+        exit(275)
 
     return response
-
-
-def show_asset_info(acct, idate, cur):
-    acct_name = acct.GetName()
-    acct_bal = acct.GetBalanceAsOfDate(idate)
-    acct_bal_dec = gnc_numeric_to_python_decimal(acct_bal)
-    acct_comm = acct.GetCommodity()
-    print("{} balance of shares on {} = {}".format(acct_name, idate, acct_bal_dec))
-
-    acct_cad = gnc_numeric_to_python_decimal(acct.ConvertBalanceToCurrencyAsOfDate(acct_bal, acct_comm, cur, idate))
-    print("{} balance on 2018-12-31 = CAD${}".format(acct_name, acct_cad))
 
 
 def update_assets_main():
@@ -205,14 +286,14 @@ def update_assets_main():
     """
     exe = argv[0].split('/')[-1]
     if len(argv) < 4:
-        print("NOT ENOUGH parameters!")
-        print("usage: {} <book url> <mode=xxx[prod][send]> <year> [quarter]".format(exe))
-        print("PROGRAM EXIT!")
+        print_error("NOT ENOUGH parameters!")
+        print_info("usage: {} <book url> <mode=xxx[prod][send]> <year> [quarter]".format(exe), GREEN)
+        print_error("PROGRAM EXIT!")
         return
 
     gnucash_file = argv[1]
     mode = argv[2].lower()
-    print("\nrunning '{}' on '{}' in mode '{}' at run-time: {}\n".format(exe, gnucash_file, mode, now))
+    print_info("\nrunning '{}' on '{}' in mode '{}' at run-time: {}\n".format(exe, gnucash_file, mode, now), GREEN)
 
     re_year = int(argv[3])
     re_quarter = int(argv[4]) if len(argv) > 4 else 0
@@ -221,7 +302,7 @@ def update_assets_main():
 
     send_assets(mode, re_year)
 
-    print("\n >>> PROGRAM ENDED.")
+    print_info("\n >>> PROGRAM ENDED.", MAGENTA)
 
 
 if __name__ == "__main__":
