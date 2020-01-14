03558ef8a3c001dd3bce88fe5c38c0afae3346d4
epistemik <epistemik@gmail.com>
2020-01-13 20:22:12 -0500

    switch to F-strings where useful

--- startUI.py
+++ startUI.py
@@ -10,7 +10,7 @@ __author_email__ = 'epistemik@gmail.com'
 __python_version__  = 3.9
 __gnucash_version__ = 3.8
 __created__ = '2019-03-30'
-__updated__ = '2020-01-11'
+__updated__ = '2020-01-13'
 
 from sys import argv, path
 path.append("/home/marksa/dev/git/Python/Gnucash/createGncTxs")
@@ -90,23 +90,23 @@ class UpdateBudgetQtrly(QDialog):
 
     # noinspection PyAttributeOutsideInit,PyUnresolvedReferences
     def create_group_box(self):
-        self.gb_main = QGroupBox("Parameters:")
+        self.gb_main = QGroupBox('Parameters:')
         layout = QFormLayout()
 
         self.cb_script = QComboBox()
         self.cb_script.addItems([x for x in MAIN_FXNS])
         self.cb_script.currentIndexChanged.connect(partial(self.script_change))
-        layout.addRow(QLabel("Script:"), self.cb_script)
+        layout.addRow(QLabel('Script:'), self.cb_script)
         self.script = self.cb_script.currentText()
 
         self.gnc_file_btn = QPushButton('Get Gnucash file')
         self.gnc_file_btn.clicked.connect(partial(self.open_file_name_dialog))
-        layout.addRow(QLabel("Gnucash File:"), self.gnc_file_btn)
+        layout.addRow(QLabel('Gnucash File:'), self.gnc_file_btn)
 
         self.cb_mode = QComboBox()
         self.cb_mode.addItems([TEST,SEND])
         self.cb_mode.currentIndexChanged.connect(partial(self.mode_change))
-        layout.addRow(QLabel("Mode:"), self.cb_mode)
+        layout.addRow(QLabel('Mode:'), self.cb_mode)
         self.mode = self.cb_mode.currentText()
 
         self.cb_domain = QComboBox()
@@ -123,27 +123,27 @@ class UpdateBudgetQtrly(QDialog):
         self.cb_dest.currentIndexChanged.connect(partial(self.selection_change, self.cb_dest, DEST))
         layout.addRow(QLabel(DEST+':'), self.cb_dest)
 
-        vert_box = QGroupBox("Check:")
+        vert_box = QGroupBox('Check:')
         vert_layout = QVBoxLayout()
-        self.ch_gnc = QCheckBox("Save Gnucash info to JSON file?")
-        self.ch_ggl = QCheckBox("Save Google info to JSON file?")
-        self.ch_debug = QCheckBox("Print DEBUG info?")
+        self.ch_gnc = QCheckBox('Save Gnucash info to JSON file?')
+        self.ch_ggl = QCheckBox('Save Google info to JSON file?')
+        self.ch_debug = QCheckBox('Print DEBUG info?')
         vert_layout.addWidget(self.ch_gnc)
         vert_layout.addWidget(self.ch_ggl)
         vert_layout.addWidget(self.ch_debug)
         vert_box.setLayout(vert_layout)
-        layout.addRow(QLabel("Options"), vert_box)
+        layout.addRow(QLabel('Options'), vert_box)
 
         self.exe_btn = QPushButton('Go!')
         self.exe_btn.clicked.connect(partial(self.button_click))
-        layout.addRow(QLabel("Execute:"), self.exe_btn)
+        layout.addRow(QLabel('Execute:'), self.exe_btn)
 
         self.gb_main.setLayout(layout)
 
     def open_file_name_dialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getOpenFileName(self, "Get Gnucash Files", "",
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Get Gnucash Files', '',
                                                    "Gnucash Files (*.gnc);;All Files (*)", options=options)
         if file_name:
             self.gnc_file = file_name
@@ -153,10 +153,10 @@ class UpdateBudgetQtrly(QDialog):
     def script_change(self):
         """must adjust domain and possibly quarter"""
         new_script = self.cb_script.currentText()
-        self.log.print_info("Script changed to: {}".format(new_script), MAGENTA)
+        self.log.print_info(F"Script changed to: {new_script}", MAGENTA)
         if new_script != self.script:
             initial_domain = self.cb_domain.currentText()
-            self.log.print_info("Start with domain = {}".format(initial_domain), BROWN)
+            self.log.print_info(F"Start with domain = {initial_domain}", BROWN)
             if new_script == REV_EXPS:
                 self.cb_domain.clear()
                 self.cb_domain.addItems(PARAMS[REV_EXPS])
@@ -177,7 +177,7 @@ class UpdateBudgetQtrly(QDialog):
                     self.cb_qtr.clear()
                     self.cb_qtr.addItem('NOT NEEDED')
                 else:
-                    raise Exception("INVALID SCRIPT!!?? '{}'".format(new_script))
+                    raise Exception(F"INVALID SCRIPT!!?? '{new_script}'")
 
             # does not seem to be any defined function to return list of current items
             if self.cb_domain.currentText() != initial_domain \
@@ -190,22 +190,22 @@ class UpdateBudgetQtrly(QDialog):
     def mode_change(self):
         """need the destination sheet if mode is Send"""
         new_mode = self.cb_mode.currentText()
-        self.log.print_info("Mode changed to '{}'.".format(new_mode), CYAN)
+        self.log.print_info(F"Mode changed to '{new_mode}'.", CYAN)
         if new_mode != self.mode:
             if new_mode == TEST:
                 self.cb_dest.clear()
             elif new_mode == SEND:
                 self.cb_dest.addItems([SHEET_1, SHEET_2])
             else:
-                raise Exception("INVALID MODE!!?? '{}'".format(new_mode))
+                raise Exception(F"INVALID MODE!!?? '{new_mode}'")
 
             self.mode = new_mode
 
     def button_click(self):
         """assemble the necessary parameters"""
-        self.log.print_info("Clicked '{}'.".format(self.exe_btn.text()))
+        self.log.print_info(F"Clicked '{self.exe_btn.text()}'.")
         exe = self.cb_script.currentText()
-        self.log.print_info("Script is '{}'.".format(exe))
+        self.log.print_info(F"Script is '{exe}'.")
 
         if not self.gnc_file:
             self.response_box.setText('>>> MUST select a Gnucash File!')
@@ -241,7 +241,7 @@ class UpdateBudgetQtrly(QDialog):
         if callable(main_fxn):
             reply = main_fxn(cl_params)
         else:
-            msg = "Problem with main??!! '{}'".format(main_fxn)
+            msg = F"Problem with main??!! '{main_fxn}'"
             self.log.print_error(msg)
             reply = msg
 
@@ -249,7 +249,7 @@ class UpdateBudgetQtrly(QDialog):
 
     def selection_change(self, cb:QComboBox, label:str):
         """info printing only"""
-        self.log.print_info("ComboBox '{}' selection changed to '{}'.".format(label, cb.currentText()), BLUE)
+        self.log.print_info(F"ComboBox '{label}' selection changed to '{cb.currentText()}'.", BLUE)
 
 
 def ui_main():
