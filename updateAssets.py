aedeed9115877483e632a4eed80a454b34e34875
epistemik <epistemik@gmail.com>
2024-07-14 13:44:20 -0400

    match new signatures of base fill_gnucash_data() and go()

--- updateAssets.py
+++ updateAssets.py
@@ -9,12 +9,11 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2019-04-06"
-__updated__ = "2024-07-10"
+__updated__ = "2024-07-12"
 
 from updateBudget import *
 
 base_run_file = get_base_filename(__file__)
-# print(base_run_file)
 
 ASSETS_DATA = {
     # first data row in the sheet
@@ -83,7 +82,7 @@ class UpdateAssets(UpdateBudget):
     def __init__(self, args:list, p_logname:str):
         super().__init__(args, p_logname)
 
-        # Google sheet to update
+        # Google sheet to update or just testing
         self.dest = self.mode
         if '1' in self.mode:
             self.dest = QTR_ASTS_SHEET
@@ -91,21 +90,21 @@ class UpdateAssets(UpdateBudget):
             self.dest = QTR_ASTS_2_SHEET
         self._lgr.debug(F"dest = {self.dest}")
 
-    def fill_gnucash_data(self, p_session:GnucashSession, p_qtr:int, p_year:str, data_qtr:dict) -> dict:
+    def fill_gnucash_data(self, p_session:GnucashSession, p_qtr:int, p_year:str):
         """
         Get ASSET data for specified year and quarter
         :param   p_session: Gnucash session reference
         :param       p_qtr: 1..4 for quarter to update
         :param      p_year: year to update
-        :param    data_qtr: dict for data
         """
         self._lgr.debug(F"find Assets in {p_session.get_file_name()} for {p_year}-Q{p_qtr}")
         start_month = (p_qtr * 3) - 2
         int_year = get_int_year( p_year, ASSETS_DATA[BASE_YEAR] )
         end_date = current_quarter_end(int_year, start_month)
 
+        data_qtr = {}
         asset_accounts = ASSET_ACCTS_NEW
-        # use slightly different accounts for 2019 onwards
+        # using slightly different accounts for 2019 onwards
         if int_year < 2019:
             asset_accounts = ASSET_ACCTS
         p_session.get_account_assets(asset_accounts, end_date, p_data=data_qtr)
@@ -114,8 +113,6 @@ class UpdateAssets(UpdateBudget):
 
         self._gnucash_data.append(data_qtr)
         self._lgr.debug(json.dumps(data_qtr, indent = 4))
-
-        return data_qtr
 
     def fill_google_data(self, p_years:list):
         """
@@ -126,8 +123,7 @@ class UpdateAssets(UpdateBudget):
         # get the row from Year and Quarter value in each item
         for item in self._gnucash_data:
             target_year = get_int_year( item[YR], ASSETS_DATA[BASE_YEAR] )
-            year_row = ASSETS_DATA[BASE_ROW] \
-                       + year_span( target_year, ASSETS_DATA[BASE_YEAR], ASSETS_DATA[YEAR_SPAN], ASSETS_DATA[HDR_SPAN] )
+            year_row = ASSETS_DATA[BASE_ROW] + year_span( target_year, ASSETS_DATA[BASE_YEAR], ASSETS_DATA[YEAR_SPAN], ASSETS_DATA[HDR_SPAN] )
             int_qtr = int(item[QTR])
             dest_row = year_row + ( (int_qtr - 1) * ASSETS_DATA[QTR_SPAN] )
             self._lgr.info(F"{item[YR]}-Q{item[QTR]} dest row = {dest_row}\n")
@@ -144,7 +140,7 @@ class UpdateAssets(UpdateBudget):
 
 def update_assets_main(args:list) -> dict:
     assets = UpdateAssets(args, base_run_file)
-    return assets.go()
+    return assets.go("Assets")
 
 
 if __name__ == "__main__":
